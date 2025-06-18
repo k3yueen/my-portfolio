30a55c7c4ea48bb626f083555f55ef234ce84006
Roza Antonevici <rozic.antonevici@me.com>
2025-06-18 20:14:13 +0200

    Create +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<main class="w-screen h-screen flex items-center justify-center flex-col">
+  <div class="m-2">
+    <h1 class="w-full text-center">ROZA ANTONEVICI</h1>
+    <p>Weird, beautiful, and interesting web design or whatever you feel like as long as it gets you hired or makes you standout to the employers</p>
+  </div>
+  <div class="w-1/2 aspect-square border-4 border-black rounded-full" />
+</main>
